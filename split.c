a05dc9600abee33f19a5deba53fbdb75f701e018
v02qabs <takesue090@gmail.com>
2024-11-19 21:30:59 +0900

    split-ftell and fscanf seek

--- split.c
+++ split.c
@@ -1,31 +1,29 @@
 #include <string.h>
 #include<stdio.h>
 
-int main(void){
-
-FILE *fp = NULL;
-int c;
-fp = fopen("Hello.cjc","r");
-while((c=fgetc(fp))!=EOF){
-	
-	char str1[50];
-	sprintf(str1, "%d", c);
-	char str2[20] = "Hello";
-	if(strcmp(str1,str2)==0){
-	
-		printf("%s", str1);
-	}
-	else{
-		printf("no");
-	}
-
-
+void fread_for(char *p, int n){
+	while(n--)
+		putchar(*p++);
+	putchar('\n');
 
 }
 
+
+
+int main(void){
+FILE *fp = NULL;
+int c;
+int pos;
+char ss[100];
+fp = fopen("Hello.cjc","r");
+int l=0;
+fscanf(fp,"%s", ss);
+fseek(fp, 0, SEEK_END);
+pos = ftell(fp);
+
 fclose(fp);
-
-
+printf("[%d]",pos);
+fread_for(ss,pos);
 char s[]="aaaaaQaa";
 int i;
 for(i=0;(s[i]!='Q' && s[i]!='\0');i++){
